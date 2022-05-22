74a1648d7c2e38a158ac0266aa136fe27acef9b8
Zhenyu <skyking_H@hotmail.com>
2022-05-22 20:46:39 +0800

    feat: update download href

--- src/Index.jsx
+++ src/Index.jsx
@@ -7,8 +7,8 @@ function Index() {
       <inner block flex-display flex-align-items-center >
         <img src="https://danwan-cdn.oss-cn-hangzhou.aliyuncs.com/global/logo.png" />
         <downloadUrls block block-margin-left-50px>
-          <version use="a" block>for mac</version>
-          <version use="a" block block-margin-top-50px>for windows 10/11</version>
+          <version use="a" href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/mac/Danwan.zip" block>for mac</version>
+          <version use="a" href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/windows/danwan.exe" block block-margin-top-50px>for windows 10/11</version>
         </downloadUrls>
       </inner>
     </homeBody>
