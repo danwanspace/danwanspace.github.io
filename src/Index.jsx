1bffafc2b5736c1ebe3751697b25d624746819f8
Zhenyu <skyking_H@hotmail.com>
2022-05-24 10:24:17 +0800

    feat: use mac zip version

--- src/Index.jsx
+++ src/Index.jsx
@@ -7,13 +7,13 @@ function Index() {
       <inner block flex-display flex-align-items-center >
         <img src="https://danwan-cdn.oss-cn-hangzhou.aliyuncs.com/global/logo.png" />
         <downloadUrls block block-margin-left-50px>
-          <version use="a" href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/mac/danwan.dmg" block>for mac</version>
-          <versionHelp href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/windows/danwan.exe" block block-margin-top-10px>
+          <version use="a" href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/mac/Danwan.zip" block>for mac</version>
+          <versionHelp block block-margin-top-10px>
             由于 danwan 还未正式上架，启动时可能出现“无法验证开发者”的问题，请看 <a target="_blank"  href="https://z8lxoxwryu.feishu.cn/docs/doccnEBdorpKm76tf8Ha3jTxgte">解决方法</a> 。
           </versionHelp>
 
           <version use="a" href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/windows/danwan.exe" block block-margin-top-50px>for windows 10/11</version>
-          <versionHelp href="https://danwan-native.oss-cn-hangzhou.aliyuncs.com/windows/danwan.exe" block block-margin-top-10px>静默安装，安装成功会在桌面生成 danwan.exe</versionHelp>
+          <versionHelp block block-margin-top-10px>静默安装，安装成功会在桌面生成 danwan.exe</versionHelp>
         </downloadUrls>
       </inner>
     </homeBody>
